54ed3cd780292494454f93ccf45c7d903640aa4f
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2025-04-01 10:30:03 +0300

    עדכון קוד

--- Train.java
+++ Train.java
@@ -1,36 +1,45 @@
 /**
  * Program that calculates the distance between two trains.
- *
+ * The user inputs the speed and time of each train,
+ * and the program calculates the absolute difference between the distances.
+ * 
+ * Example output: "The distance between the trains is 5.0 km."
+ * 
  * @author (Eylon Maayan)
- * @version (mamn 11 )
+ * @version (mamn 11)
  */
-import java.util.Scanner;
 
-public class Train {
-    public static void main (String[] args) {
-        // Scanning data from the user
-        Scanner scan = new Scanner(System.in);
+ import java.util.Scanner;
 
-        System.out.println("Please enter 4 integers:");
-        System.out.println("Please enter the speed of train 1:");
-        int v1 = scan.nextInt();
-
-        System.out.println("Please enter the time of train 1:");
-        int t1 = scan.nextInt();
-
-        System.out.println("Please enter the speed of train 2:");
-        int v2 = scan.nextInt();
-
-        System.out.println("Please enter the time of train 2:");
-        int t2 = scan.nextInt();
-
-        // Calculate distance of each train
-        int d1 = v1 * t1;
-        int d2 = v2 * t2;
-
-        // Calculate the distance between them
-        int distance = Math.abs(d2 - d1);
-
-        System.out.println("The distance between the two trains is " + distance + " km");
-    }
-}
+ public class Train {
+     public static void main(String[] args) {
+         // Create a Scanner object to get input from the user
+         Scanner scan = new Scanner(System.in);
+ 
+         // Prompt the user to enter 4 integers: speed and time for both trains
+         System.out.println("Please enter 4 integers:");
+         
+         System.out.println("Please enter the speed of train 1:");
+         int v1 = scan.nextInt();
+ 
+         System.out.println("Please enter the time of train 1:");
+         int t1 = scan.nextInt();
+ 
+         System.out.println("Please enter the speed of train 2:");
+         int v2 = scan.nextInt();
+ 
+         System.out.println("Please enter the time of train 2:");
+         int t2 = scan.nextInt();
+ 
+         // Calculate the distance traveled by each train
+         double d1 = v1 * t1;
+         double d2 = v2 * t2;
+ 
+         // Calculate the absolute difference between the distances
+         double distance = Math.abs(d2 - d1);
+ 
+         // Print the result with one decimal point (even if it's ".0")
+         System.out.printf("The distance between the trains is %.1f km.\n", distance);
+     }
+ }
+ 
